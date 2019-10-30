29c4823744749c450030a2c74ac3ac3b0ca9d862
github username <github email address>
2019-10-30 16:03:09 -0700

    did some refactoring and made a large if-else statement. For 36 it's returning XVI, so it's only counting the 10's place once.

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 function convertToRoman(num) {
-    let roman = [I, V, X, L, C, D, M];
-    let values = [1, 5, 10, 50, 100, 500, 1000];
-    let final = [];
+  let roman = ["M", "D", "C", "L", "X", "V", "I"];
+  let values = [1000, 500, 100, 50, 10, 5, 1];
+  let final = [];
 
+  for (let i = 0; i < roman.length; i++) {
+    console.log(num > values[i]);
 
-  return num;
+    if (num > 1000 && values[i] === 1000) {
+      final.push(roman[i]);
+      num = num - 1000;
+    } else if (num > 500 && values[i] === 500) {
+      final.push(roman[i]);
+      num = num - 500;
+    } else if (num > 100 && values[i] === 100) {
+      final.push(roman[i]);
+      num = num - 100;
+    } else if (num > 50 && values[i] === 50) {
+      final.push(roman[i]);
+      num = num - 50;
+    } else if (num > 10 && values[i] === 10) {
+      final.push(roman[i]);
+      num = num - 10;
+    } else if (num > 5 && values[i] === 5) {
+      final.push(roman[i]);
+      num = num - 5;
+    } else if (num > 1 && values[i] === 1) {
+      final.push(roman[i]);
+      num = num - 1;
+    }
+  }
+
+  return final;
 }
 
 convertToRoman(36);
