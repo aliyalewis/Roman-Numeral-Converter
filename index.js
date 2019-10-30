15e1f6889c28cf1374c231f7dff947c03c4f8455
github username <github email address>
2019-10-30 15:19:31 -0700

    made two arrays, one with the roman numerals and the other with the corresponding values.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function convertToRoman(num) {
+    let roman = [I, V, X, L, C, D, M];
+    let values = [1, 5, 10, 50, 100, 500, 1000];
+    let final = [];
+
+
   return num;
 }
 
 convertToRoman(36);
+
+// I	1
+// V	5
+// X	10
+// L	50
+// C	100
+// D	500
+// M	1000
+
+// XXXVI = 36
